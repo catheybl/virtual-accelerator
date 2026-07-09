8ce3fc50eb7cea7491d0fca45a67b9641953ab02
t18 <baileytl1@ornl.gov>
2026-07-09 10:26:50 -0400

    Fixed issues with Wire.py example

--- virtaccl/examples/Wire.py
+++ virtaccl/examples/Wire.py
@@ -7,19 +7,26 @@
 # Reworked (Thomas Bailey) for overhaul of wire scanner in virac.
 
 from epics import caget, caput
+from time import sleep
 
 ws = 'MEBT_Diag:WS14'
 position = f'{ws}:Position'
 command = f'{ws}:Command'
 x = f'{ws}:Hor_Cont'
 y = f'{ws}:Ver_Cont'
+rep_rate = caget(f'{ws}:BeamRepRate')
 
 caput(command, 21)
 
 print(f'{"Position":^12s}  {"x":^8s}  {"y":^8s}')
-for i in range(32):
+while caget(command) == 21:
     p = caget(position)
     charge_x = caget(x)
     charge_y = caget(y)
-    sleep(1)
+    sleep(rep_rate)
     print(f'{p:12.2f}  {charge_x:8.3f}  {charge_y:8.3f}')
+p = caget(position)
+charge_x = caget(x)
+charge_y = caget(y)
+print(f'{p:12.2f}  {charge_x:8.3f}  {charge_y:8.3f}')
+print('Done')
