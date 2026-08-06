aeeba01a66fbf093d5161b0fdb668d794b20509e
austin-hoover <austin.michael.hoover@gmail.com>
2026-08-06 12:57:38 -0400

    IDMP formatting and organization

--- virtaccl/site/SNS_IDmp/virtual_accelerator.py
+++ virtaccl/site/SNS_IDmp/virtual_accelerator.py
@@ -2,70 +2,90 @@ import json
 import sys
 from pathlib import Path
 
-from virtaccl.PyORBIT_Model.pyorbit_va_nodes import BPMclass, WSclass, ScreenClass
-from virtaccl.PyORBIT_Model.pyorbit_virtual_accelerator import PyorbitVirtualAcceleratorBuilder, add_pyorbit_arguments
+from virtaccl.PyORBIT_Model.pyorbit_va_nodes import BPMclass
+from virtaccl.PyORBIT_Model.pyorbit_va_nodes import WSclass
+from virtaccl.PyORBIT_Model.pyorbit_va_nodes import ScreenClass
+from virtaccl.PyORBIT_Model.pyorbit_virtual_accelerator import PyorbitVirtualAcceleratorBuilder
+from virtaccl.PyORBIT_Model.pyorbit_virtual_accelerator import add_pyorbit_arguments
 
-from virtaccl.site.SNS_Linac.virtual_devices import (Quadrupole, Corrector, Quadrupole_Power_Supply,
-                                                     Corrector_Power_Supply, WireScanner, BPM, Screen)
-from virtaccl.site.SNS_Linac.virtual_devices_SNS import SNS_Dummy_BCM, SNS_Dummy_ICS
+from virtaccl.site.SNS_Linac.virtual_devices import Quadrupole
+from virtaccl.site.SNS_Linac.virtual_devices import Corrector
+from virtaccl.site.SNS_Linac.virtual_devices import Quadrupole_Power_Supply
+from virtaccl.site.SNS_Linac.virtual_devices import Corrector_Power_Supply
+from virtaccl.site.SNS_Linac.virtual_devices import WireScanner
+from virtaccl.site.SNS_Linac.virtual_devices import BPM
+from virtaccl.site.SNS_Linac.virtual_devices import Screen
+from virtaccl.site.SNS_Linac.virtual_devices_SNS import SNS_Dummy_BCM
+from virtaccl.site.SNS_Linac.virtual_devices_SNS import SNS_Dummy_ICS
 
 from virtaccl.PyORBIT_Model.pyorbit_lattice_controller import OrbitModel
 from virtaccl.beam_line import BeamLine
-from virtaccl.EPICS_Server.ca_server import EPICS_Server, add_epics_arguments
+from virtaccl.EPICS_Server.ca_server import EPICS_Server
+from virtaccl.EPICS_Server.ca_server import add_epics_arguments
 from virtaccl.virtual_accelerator import VA_Parser
 
-from virtaccl.site.SNS_IDmp.IDmp_maker import get_IDMP_lattice_and_bunch
+from .maker import get_lattice
+from .maker import get_bunch
 
 
-def idmp_arguments():
+def parse_args():
     loc = Path(__file__).parent
     va_parser = VA_Parser()
-    va_parser.set_description('Run the SNS Injection Dump PyORBIT virtual accelerator server.')
-
+    va_parser.set_description("Run the SNS Injection Dump PyORBIT virtual accelerator server.")
     va_parser = add_pyorbit_arguments(va_parser)
+
     # Set the defaults for the PyORBIT model.
-    va_parser.remove_argument('--lattice')
-    va_parser.remove_argument('--start')
-    va_parser.remove_argument('--drift_length')
-    va_parser.remove_argument('end')
-    va_parser.remove_argument('--bunch')
+    va_parser.remove_argument("--lattice")
+    va_parser.remove_argument("--start")
+    va_parser.remove_argument("--drift_length")
+    va_parser.remove_argument("end")
+    va_parser.remove_argument("--bunch")
 
     va_parser = add_epics_arguments(va_parser)
 
     # Json file that contains a dictionary connecting EPICS name of devices with their associated element model names.
-    va_parser.add_argument('--config_file', '-f', default=loc / 'va_config.json', type=str,
-                           help='Pathname of config json file.')
+    va_parser.add_argument(
+        "--config_file",
+        "-f",
+        default=loc / "va_config.json",
+        type=str,
+        help="Pathname of config json file.",
+    )
 
     # Json file that contains a dictionary connecting EPICS name of devices with their phase offset.
-    va_parser.add_argument('--phase_offset', default=None, type=str,
-                           help='Pathname of phase offset file.')
+    va_parser.add_argument(
+        "--phase_offset", default=None, type=str, help="Pathname of phase offset file."
+    )
 
     va_args = va_parser.initialize_arguments()
     return va_args
 
 
-def build_idmp(**kwargs):
+def build(**kwargs):
     kwargs = idmp_arguments() | kwargs
 
-    debug = kwargs['debug']
+    debug = kwargs["debug"]
 
-    config_file = Path(kwargs['config_file'])
+    config_file = Path(kwargs["config_file"])
     with open(config_file, "r") as json_file:
         devices_dict = json.load(json_file)
 
-    part_num = kwargs['particle_number']
-    lattice, bunch = get_IDMP_lattice_and_bunch(part_num, x_off=2, xp_off=0.3, debug=debug)
+    part_num = kwargs["particle_number"]
+
+    latttice = get_lattice(debug=debug)
+    bunch = get_bunch(part_num, x_off=0.002, xp_off=0.0003, debug=debug)
+
     model = OrbitModel(input_bunch=bunch, debug=debug)
     model.define_custom_node(BPMclass.node_type, BPMclass.parameter_list, diagnostic=True)
     model.define_custom_node(WSclass.node_type, WSclass.parameter_list, diagnostic=True)
     model.define_custom_node(ScreenClass.node_type, ScreenClass.parameter_list, diagnostic=True)
-    model.set_beam_current(38.0e-3)  # Set the initial beam current in Amps.
+    model.set_beam_current(0.038)
     model.initialize_lattice(lattice)
     element_list = model.get_element_list()
 
     beam_line = BeamLine()
 
-    offset_file = kwargs['phase_offset']
+    offset_file = kwargs["phase_offset"]
     if offset_file is not None:
         with open(offset_file, "r") as json_file:
             offset_dict = json.load(json_file)
@@ -76,12 +96,14 @@ def build_idmp(**kwargs):
         ele_name = device_dict["PyORBIT_Name"]
         polarity = device_dict["Polarity"]
         if ele_name in element_list:
-            initial_field = abs(model.get_element_parameters(ele_name)['dB/dr'])
+            initial_field = abs(model.get_element_parameters(ele_name)["dB/dr"])
             if "Power_Supply" in device_dict and device_dict["Power_Supply"] in quad_ps:
                 ps_name = device_dict["Power_Supply"]
                 ps_device = Quadrupole_Power_Supply(ps_name, initial_field)
                 beam_line.add_device(ps_device)
-                corrector_device = Quadrupole(name, ele_name, power_supply=ps_device, polarity=polarity)
+                corrector_device = Quadrupole(
+                    name, ele_name, power_supply=ps_device, polarity=polarity
+                )
                 beam_line.add_device(corrector_device)
 
     corr_ps = devices_dict["Corrector_Power_Supply"]
@@ -90,12 +112,14 @@ def build_idmp(**kwargs):
         ele_name = device_dict["PyORBIT_Name"]
         polarity = device_dict["Polarity"]
         if ele_name in element_list:
-            initial_field = model.get_element_parameters(ele_name)['B']
+            initial_field = model.get_element_parameters(ele_name)["B"]
             if "Power_Supply" in device_dict and device_dict["Power_Supply"] in corr_ps:
                 ps_name = device_dict["Power_Supply"]
                 ps_device = Corrector_Power_Supply(ps_name, initial_field)
                 beam_line.add_device(ps_device)
-                corrector_device = Corrector(name, ele_name, power_supply=ps_device, polarity=polarity)
+                corrector_device = Corrector(
+                    name, ele_name, power_supply=ps_device, polarity=polarity
+                )
                 beam_line.add_device(corrector_device)
 
     wire_scanners = devices_dict["Wire_Scanner"]
@@ -119,12 +143,12 @@ def build_idmp(**kwargs):
             screen_device = Screen(name, model_name)
             beam_line.add_device(screen_device)
 
-    dummy_device = SNS_Dummy_BCM("Ring_Diag:BCM_D09", 'HEBT_Diag:BPM11')
+    dummy_device = SNS_Dummy_BCM("Ring_Diag:BCM_D09", "HEBT_Diag:BPM11")
     beam_line.add_device(dummy_device)
     dummy_device = SNS_Dummy_ICS("ICS_Tim")
     beam_line.add_device(dummy_device)
 
-    delay = kwargs['ca_proc']
+    delay = kwargs["ca_proc"]
     server = EPICS_Server(process_delay=delay)
 
     idmp_virac = PyorbitVirtualAcceleratorBuilder(model, beam_line, server, **kwargs)
@@ -132,10 +156,10 @@ def build_idmp(**kwargs):
 
 
 def main():
-    args = idmp_arguments()
-    idmp = build_idmp(**args).build()
-    idmp.start_server()
+    args = parse_args()
+    va = build(**args).build()
+    va.start_server()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
