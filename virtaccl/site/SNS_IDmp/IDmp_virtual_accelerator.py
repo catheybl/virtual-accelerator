b2d497778edc99d30957b70409da4ba861398bb6
t18 <baileytl1@ornl.gov>
2026-08-06 12:10:00 -0400

    Passed rep rate and device frequency into wire scanner on Injection Dump

--- virtaccl/site/SNS_IDmp/IDmp_virtual_accelerator.py
+++ virtaccl/site/SNS_IDmp/IDmp_virtual_accelerator.py
@@ -65,6 +65,9 @@ def build_idmp(**kwargs):
 
     beam_line = BeamLine()
 
+    refresh_rate = kwargs['refresh_rate']
+    update_frequency = kwargs['device_frequency']
+
     offset_file = kwargs['phase_offset']
     if offset_file is not None:
         with open(offset_file, "r") as json_file:
@@ -101,7 +104,10 @@ def build_idmp(**kwargs):
     wire_scanners = devices_dict["Wire_Scanner"]
     for name, model_name in wire_scanners.items():
         if model_name in element_list:
-            ws_device = WireScanner(name, model_name)
+            # Passing refresh rate to the WireScanner device for velocity calculations.
+            ws_device = WireScanner(name, model_name, {
+                'refresh_rate': refresh_rate,
+                'update_frequency': update_frequency})
             beam_line.add_device(ws_device)
 
     bpms = devices_dict["BPM"]
